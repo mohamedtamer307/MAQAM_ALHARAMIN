f233fd9a6097ebf76025095c61ff652ace273913
mohamedtamer307 <131061509+mohamedtamer307@users.noreply.github.com>
2026-06-09 20:20:35 +0300

    Add files via upload

---
+++ bookings-supabase.js
@@ -0,0 +1,11 @@
+const supabaseUrl =
+"https://ddyvcsttnrzgrrzhlxji.supabase.co";
+
+const supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkeXZjc3R0bnJ6Z3JyemhseGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0Mjc4MjcsImV4cCI6MjA5NjAwMzgyN30.PB2Ngo8seVNICNauAmhbmA54F5bqtCNVmwqokjNfi1U";
+
+const supabaseClient =
+window.supabase.createClient(
+supabaseUrl,
+supabaseKey
+);
